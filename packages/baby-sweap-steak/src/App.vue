59ddd41887de2b9df56cd9bcdddb192cd3ad2993
Joe Wood <joe.wood@bbc.co.uk>
2019-01-14 18:03:22 +0000

    Initialize the project and flesh out request

--- packages/baby-sweap-steak/src/App.vue
+++ packages/baby-sweap-steak/src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <header id="header">
+      <h1>baby sweep steak</h1>
+    </header>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <GetTimeSlotForm/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import GetTimeSlotForm from "./components/GetTimeSlotForm.vue";
+import VueResource from "vue-resource";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    GetTimeSlotForm
+  },
+  data: function () {
+    return {
+      sources: ''
+    }
+  },
+  methods: {
+    getUnits: function() {}
+  },
+  created: function() {
+    console.log(process.env.VUE_APP_BASE_API);
+    // console.log("This is potential passed data: " + data);
+    this.$http.get(process.env.VUE_APP_BASE_API).then(response => {
+      this.sources = response.data.sources;
+      console.log(response.data)
+    });
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#header {
+  position: relative;
+  background-color: #448cff;
+  height: 100px;
+  top: 50%;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
---
+++ packages/baby-sweap-steak/src/components/GetTimeSlotForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="main">
+    <h1>Please generate your timeslot</h1>
+    <h2>It will be a random hour slot chosen out of the slots available</h2>
+    <form @submit.prevent="handleSubmit">
+      <label>
+        BBC login:
+        <input type="name" v-model="user.name">
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+    <h2>Your time slot is: {{ timeslot }}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "GetTimeSlotForm",
+  data: function() {
+    return {
+      message: "sdfg",
+      timeslot: "",
+      user: {
+        name: ""
+      }
+    };
+  },
+  methods: {
+    handleSubmit(data) {
+      console.log(process.env.VUE_APP_BASE_API);
+      console.log("This is potential passed data: " + data)
+      this.$http.post(process.env.VUE_APP_BASE_API).then(
+        response => {
+          // get body data
+          this.someData = response.body;
+          console.log(this.sameData)
+        },
+        response => {
+          // error callback
+        }
+      );
+
+      console.log("this has submitted now.");
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main {
+  /* padding: 10%; */
+}
+.p {
+}
+</style>
