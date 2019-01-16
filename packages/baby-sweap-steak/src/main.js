9d0e6c2a5cc50991769a764fa6d3ef20ad738da6
Joe Wood <joe.wood@bbc.co.uk>
2019-01-16 17:14:09 +0000

    Remove unnessary vuex from manager

--- packages/baby-sweap-steak/src/main.js
+++ packages/baby-sweap-steak/src/main.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
 import 'es6-promise/auto'
-import Vuex from 'vuex'
 
 Vue.config.productionTip = false
-Vue.use(Vuex)
 Vue.use(VueResource);
 
 new Vue({
